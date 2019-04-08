670bc599cddbf92baddc58f9d2ae19c95163191f
Julie K <juliette.nada@gmail.com>
2019-04-08 12:03:06 +0200

    00 and 01 prog created

---
+++ lib/01_temperature.rb
@@ -0,0 +1,15 @@
+# takes a f degree temperature, converts to float then converts to c degre
+# only return integer
+def ftoc(ftemp)
+  ctemp = (ftemp.to_f - 32) * 5 / 9
+  return ctemp.to_i
+end
+
+# takes a c degree temperature, converts to float then converts to f degre
+# returns float
+def ctof(ctemp)
+  ftemp = ctemp.to_f * 9 / 5 +32
+  return ctemp.to_f
+end
+
+# doubt : does the ctof return integer or float ???
